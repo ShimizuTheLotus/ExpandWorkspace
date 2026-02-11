eec19a8a2dfb948baed068b1fa02fb6cc6819611
ShimizuTheLotus <152269217+ShimizuTheLotus@users.noreply.github.com>
2026-02-11 17:53:27 +0800

    Prevent opening multi instances.

--- ExpandWorkspace/App.xaml.cs
+++ ExpandWorkspace/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using System.Windows;
 
+
 namespace ExpandWorkspace
 {
     /// <summary>
@@ -9,6 +10,25 @@ namespace ExpandWorkspace
     /// </summary>
     public partial class App : System.Windows.Application
     {
+        System.Threading.Mutex? mutex;
+
+        public App()
+        {
+            this.Startup += new StartupEventHandler(App_Startup);
+        }
+
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            bool ret;
+            mutex = new System.Threading.Mutex(true, "ShimizuTheLotus.ExpandWorkspace", out ret);
+
+            if (!ret)
+            {
+                System.Windows.MessageBox.Show("ExpandWorkspace已有一个程序实例运行");
+                Environment.Exit(0);
+            }
+
+        }
     }
 
 }
